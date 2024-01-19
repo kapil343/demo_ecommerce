be5bc981d01a3f784d4b6e5ec5a50551a23c5d62
kapil343 <kapil.patel@codiatic.com>
2024-01-19 12:32:18 +0530

    {[admin_order_functionality] : 1. seller can add discount on his products only, 2. one product can only have one discount}

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,4 +1,38 @@
 class DiscountsController < ApplicationController
-  def index
-  end
+    def index
+      @discounts = Discount.all.page(params[:page])
+    end
+    def new
+      @discount = Discount.new
+    end
+    def create
+      @discount = Discount.new(discount_params)
+      if @discount.save
+        redirect_to discounts_path
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def edit
+      @discount = Discount.find(params[:id])
+    end
+    def update
+      @discount = Discount.find(params[:id])
+      if @discount.update(discount_params)
+        redirect_to discounts_path
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+    def destroy
+      @discount = Discount.find(params[:id])
+      @discount.destroy
+      redirect_to discounts_path
+    end
+
+    private
+    def discount_params
+      params.require(:discount).permit(:percentage, :expiry_date, :product_id)
+    end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,14 @@ class Product < ApplicationRecord
   has_one_attached :product_image
   belongs_to :user
   belongs_to :category
-  has_many :discounts
+  has_one :discount
+
+  def discounted_price product
+    discount = product.discount
+      if discount
+        product.price - (product.price * discount.percentage / 100)
+      else
+        product.price
+      end
+  end
 end

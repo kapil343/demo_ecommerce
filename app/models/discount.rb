e9828436bda7adbf52f237c690ef555cec941c7e
kapil343 <kapil.patel@codiatic.com>
2024-01-19 12:38:35 +0530

    Revert "{[admin_order_functionality] : 1. seller can add discount on his products only, 2. one product can only have one discount}"
    This reverts commit be5bc981d01a3f784d4b6e5ec5a50551a23c5d62.

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,13 +1,3 @@
 class Discount < ApplicationRecord
   belongs_to :product
-  paginates_per 5
-  validate :unique_discount_for_product
-
-  private
-
-  def unique_discount_for_product
-    if product&.discount.present?
-      errors.add(:product_id, 'already has a discount')
-    end
-  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,14 +4,5 @@ class Product < ApplicationRecord
   has_one_attached :product_image
   belongs_to :user
   belongs_to :category
-  has_one :discount
-
-  def discounted_price product
-    discount = product.discount
-      if discount
-        product.price - (product.price * discount.percentage / 100)
-      else
-        product.price
-      end
-  end
+  has_many :discounts
 end

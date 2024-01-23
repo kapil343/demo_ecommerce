230e4df71272c1a9b3c77aeabc643d2d48e51307
kapil343 <kapil.patel@codiatic.com>
2024-01-23 12:55:14 +0530

    added admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user = User.create(email: "kapil.patel@codiatic.com", password: 123456, confirm_password: 123456)
+user.add_role :admin

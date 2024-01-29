b9f1d2e58fcc9439e06a393bb25a5b665fa310c6
kapil343 <kapil.patel@codiatic.com>
2024-01-29 18:08:31 +0530

    added review

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_one :discount
+  has_many :reviews
 
   def discounted_price product
     discount = product.discount
